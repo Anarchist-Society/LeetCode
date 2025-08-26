7b4eba3903394a54b2c612f0c1d01282b7728e27
Anarchist-Society <220625772+Anarchist-Society@users.noreply.github.com>
2025-08-26 20:23:14 +0200

    Time: 44 ms (38.5%), Space: 44.7 MB (89.91%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,15 +1,12 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        int[] output = new int[2];
         for (int i = 0; i < nums.length; i++) {
             for (int j = i + 1; j < nums.length; j++) {
-                if (nums[i] + nums[j] == target) {
-                    output[0] = i;
-                    output[1] = j;
-                    break;
+                if (nums[j] == target - nums[i]) {
+                    return new int[] {i,j};
                 }
             }
         }
-        return output;
+        return new int[] {};
     }
 }
