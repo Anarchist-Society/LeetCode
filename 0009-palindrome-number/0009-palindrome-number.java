fda7a32b29795cd16066bd9a1250d7add5149708
Anarchist-Society <220625772+Anarchist-Society@users.noreply.github.com>
2025-08-28 17:42:17 +0200

    Time: 5 ms (65.57%), Space: 44.5 MB (17.03%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.java
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -1,16 +1,16 @@
 class Solution {
     public boolean isPalindrome(int x) {
-        if (x < 0) {
+        if (x < 0 || x % 10 == 0 && x != 0) {
             return false;
         }
 
-        int temp = x;
         int reverse = 0;
-        while (temp != 0) {
-            reverse = (reverse * 10) + (temp % 10);
-            temp /= 10;
+        while (x > reverse) {
+            reverse = (reverse * 10) + (x % 10); 
+            x /= 10;
         }
-        
-        return x == reverse;
+
+        return (x == reverse || x == reverse / 10);
+
     }
 }
