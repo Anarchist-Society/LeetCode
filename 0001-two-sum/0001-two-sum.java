f786450585dbbd21eb9b5984dfb3322bb15ff295
Anarchist-Society <220625772+Anarchist-Society@users.noreply.github.com>
2025-08-28 04:03:29 +0200

    Time: 2 ms (98.89%), Space: 45.4 MB (5.11%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,25 +1,13 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-                Map<Integer, Integer> map = new HashMap<>();
-        // En el primer bucle lo que hacemos es guardar los valores y los indices en el HashMap
-        // Key: nums[i] i -> índice del array
-        // Value: i -> índice del array
+        Map<Integer, Integer> map = new HashMap<>();
         for (int i = 0; i < nums.length; i++) {
+            int objetivo = target - nums[i]; // Almacenamos en una variable el segundo sumando que buscamos
+            if (map.containsKey(objetivo)) {
+                return new int[] {map.get(objetivo), i};
+            }
             map.put(nums[i], i);
         }
-
-        // En el segundo bucle comprobamos si está en el HashMap el segundo sumando (target - nums[i]) de cada elemento del array nums
-        for (int i = 0; i < nums.length; i++) {
-            int objetivo = target - nums[i];
-            // Comprobamos si está el segundo sumando (map.containsKey(objetivo)) y si ese sumando no es el mismo que el primer sumando (map.get(objetivo))
-            // Si cumple las condiciones devolvemos un nuevo array con el primer sumando y el segundo sumando
-            if (map.containsKey(objetivo) && map.get(objetivo) != i) {
-                return new int[] {i, map.get(objetivo)};
-            }
-        }
-
-        // Si no se encuentra un par válido devuelve una matriz vacía en lugar de NULL.
         return new int[] {};
-
     }
 }
