441c7a724939c6fe743440aebed12344fe4b046c
Anarchist-Society <220625772+Anarchist-Society@users.noreply.github.com>
2025-09-01 01:00:49 +0200

    Time: 5 ms (55.44%), Space: 45 MB (34.84%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.java
+++ 0013-roman-to-integer/0013-roman-to-integer.java
@@ -1,20 +1,22 @@
 class Solution {
     public int romanToInt(String s) {
-        Map<Character, Integer> map = Map.of('I', 1, 'V', 5, 'X', 10, 'L', 50, 'C', 100, 'D', 500, 'M', 1000);
+        Map<Character, Integer> map = new HashMap<>();
+        map.put('I', 1);
+        map.put('V', 5);
+        map.put('X', 10);
+        map.put('L', 50);
+        map.put('C', 100);
+        map.put('D', 500);
+        map.put('M', 1000);
 
         int numero = 0;
-        for (int i = 0; i < s.length(); i++) {
-            int actual = map.get(s.charAt(i));
-
-            int siguiente = (i + 1 < s.length()) ? map.get(s.charAt(i + 1)) : 0;
-
-            if (actual < siguiente) {
-                numero -= actual;
+        for (int i = 0; i < s.length() - 1; i++) {
+            if (map.get(s.charAt(i)) < map.get(s.charAt(i + 1))) {
+                numero -= map.get(s.charAt(i));
             } else {
-                numero += actual;
+                numero += map.get(s.charAt(i));
             }
         }
-        return numero;
-
+        return numero + map.get(s.charAt(s.length() - 1));
     }
 }
