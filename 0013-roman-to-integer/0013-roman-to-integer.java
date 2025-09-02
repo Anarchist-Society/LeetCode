a0974060264e68c6b358fa56eb5d891c73d77770
Anarchist-Society <220625772+Anarchist-Society@users.noreply.github.com>
2025-09-02 18:08:58 +0200

    Time: 3 ms (80.57%), Space: 45 MB (34.91%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.java
+++ 0013-roman-to-integer/0013-roman-to-integer.java
@@ -1,42 +1,28 @@
 class Solution {
     public int romanToInt(String s) {
         int resultado = 0;
-        int numero = 0;
+        for (int i = 0; i < s.length(); i++)  {
+            int valorActual = getValue(s.charAt(i));
 
-        for (int i = s.length() - 1; i >= 0; i--) {
-            switch (s.charAt(i)) {
-                case 'I':
-                    numero = 1;
-                    break;
-                case 'V':
-                    numero = 5;
-                    break;
-                case 'X':
-                    numero = 10;
-                    break;
-                case 'L':
-                    numero = 50;
-                    break;
-                case 'C':
-                    numero = 100;
-                    break;
-                case 'D':
-                    numero = 500;
-                    break;
-                case 'M':
-                    numero = 1000;
-                    break;
-                default:
-                    break;
-            }
-
-            if (4 * numero < resultado) {
-                resultado -= numero; 
+            if (i < s.length() - 1 && valorActual < getValue(s.charAt(i + 1))) {
+                resultado -= valorActual; 
             } else {
-                resultado += numero;
+                resultado += valorActual;
             }
         }
-
         return resultado;
+    }
+
+        public static int getValue(char c) {
+        switch (c) {
+            case 'I': return 1; 
+            case 'V': return 5;
+            case 'X': return 10;
+            case 'L': return 50;
+            case 'C': return 100;
+            case 'D': return 500;
+            case 'M': return 1000;
+            default: return 0;
+        }
     }
 }
